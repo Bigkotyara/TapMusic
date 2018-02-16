5cbea71336ec88ce2d630f1769b4652488c5547e
Nikita Nikiforov <nnikifor@ucsd.edu>
2018-02-15 17:49:13 -0800

    Added login screen (temp)

--- public/js/helper.js
+++ public/js/helper.js
@@ -37,3 +37,12 @@ function onload() {
                                  
   
   };
+
+
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
